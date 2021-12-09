7916af763f308a329b31b0a26a3c0e73561f0584
Max Fischer <maxfischer2781@gmail.com>
2021-12-09 17:50:45 +0100

    data directory is configurable

--- aoc2021/__main__.py
+++ aoc2021/__main__.py
@@ -30,6 +30,7 @@ SOLUTIONS = dict(enumerate((day1, day2, day3, day4, day5, day6, day7, day8, day9
 
 
 def format_duration(delta: float):
+    """Format a duration in seconds as a 3-digit SI unit duration"""
     for symbol in ("s", "ms", "μs", "ns"):
         if delta > 0.5:
             break
@@ -37,10 +38,10 @@ def format_duration(delta: float):
     return f"{delta:.2f} {symbol}"
 
 
-def run_solution(day: int, example: bool):
+def run_solution(day: int, example: bool, data_dir: pathlib.Path):
     print(f"[> ### Day {day:3d} ### <]")
     data_file = f'day{day}_ex.txt' if example else f'day{day}.txt'
-    input_path = pathlib.Path(__file__).parent.parent / 'data' / data_file
+    input_path = data_dir / data_file
     data = io.StringIO(input_path.read_text())
     solver, template = SOLUTIONS[day].solve, SOLUTIONS[day].FORMAT.strip()
     pre = time.time()
@@ -61,10 +62,17 @@ CLI.add_argument(
 CLI.add_argument(
     "-e",
     "--example",
-    action="store_true"
+    action="store_true",
+    help="use example data"
+)
+CLI.add_argument(
+    "--data",
+    type=pathlib.Path,
+    default=pathlib.Path(__file__).parent.parent / 'data',
+    help="path to directory in which data is stored"
 )
 
 opts = CLI.parse_args()
 for d in opts.DAY or [max(SOLUTIONS)]:
     assert d in SOLUTIONS, f"days {', '.join(map(str, SOLUTIONS))} available, not {d}"
-    run_solution(d, opts.example)
+    run_solution(d, opts.example, opts.data)
