1c4a8482554d22729ac996355024e96e59ed6d5e
Max Fischer <maxfischer2781@gmail.com>
2021-12-10 18:01:58 +0100

    shortened day 7

--- aoc2021/day7.py
+++ aoc2021/day7.py
@@ -24,12 +24,11 @@ def triangular_cost(center, positions):
     return sum((n := abs(pos - center)) * (n + 1) // 2 for pos in positions)
 
 
-# We could do this more "efficiently" using `min` and the cost as its `key` function.
-# But since its a stupid brute-force approach, there's little point polishing it...
+# We could probably do this more efficiently using bisection or descent...
 def least_cost(start_positions: list[int], cost):
-    costs = [
-        (cost(center, start_positions), center)
-        for center in range(min(start_positions), max(start_positions))
-    ]
-    costs.sort()
-    return costs[0][0]
+    return min(
+        map(
+            lambda center: cost(center, start_positions),
+            range(min(start_positions), max(start_positions)),
+        )
+    )
